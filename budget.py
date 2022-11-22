17fdd9345d944feec0d2f103c815aa653494461c
biometricPsychography <cyclinglinguist@gmail.com>
2022-11-21 19:45:48 -0600

    Partially implement format of print(instance of Category)

--- budget.py
+++ budget.py
@@ -1,3 +1,4 @@
+
 class Category :
     
 
@@ -69,15 +70,38 @@ class Category :
         else :
             return True
     
-    def get_raw_ledger(self) :
-        return self.ledger
-    
+    def __str__(self) :
+        self.title_scaffold = '******************************'
 
+        # determine name placement in title_scaffold
+        middle_offset = int(len(self.name) / 2)
+        odd_off_extra_padding = 0
+        if len(self.name) % 2 != 0 :
+            odd_off_extra_padding = 1
+            
+        middle_index_int = 15
+        title_left = self.title_scaffold[:middle_index_int - middle_offset - odd_off_extra_padding]
+        title_right = self.title_scaffold[middle_index_int + middle_offset:]
+        self.title = title_left + self.name + title_right
+
+        self.ledger_line_scaffold = '                              '
+
+
+        for item in self.ledger :
+            item_ledger_string = item
+            item_overflow = item['description'][28:]
+
+            if len(item_overflow) > 0 :
+                item_ledger_string = item['description'][:28]
+            print(item_ledger_string)
+
+        return ''
 
 
 vg_budget = Category('Video Games')
 vg_budget.deposit(127.72, 'Work')
 vg_budget.withdraw(5)
+vg_budget.deposit(2, 'HOLY 2 DOLLAR BILL ENCHANTED BY ELVES')
 
 food_budget = Category('Food')
 food_budget.deposit(100, 'Work')
@@ -87,4 +111,5 @@ vg_budget.transfer(30, food_budget)
 print(vg_budget.ledger)
 print(food_budget.ledger)
 
+print(vg_budget)
 # def create_spend_chart(categories):
